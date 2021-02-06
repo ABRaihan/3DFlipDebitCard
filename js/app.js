6b0e99a604e889118e21ca061dfa7e0986e406e3
ABRaihan <abraihan7244@gmail.com>
2021-02-06 22:12:43 +0600

    change font style and add cycling effect

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@
 const card = document.querySelector('.card');
 const front = document.querySelector('.card .front');
 const back = document.querySelector('.card .back');
-
+let degree = 0;
 card.addEventListener('click', function (e) {
-    if (front.style.transform === '') {
-        front.style.transform = 'rotateY(180deg)';
-        back.style.transform = 'rotateY(360deg)';
-    } else {
-        front.style.transform = '';
-        back.style.transform = '';
-    }
+    cyclicalEffect()
 });
+
+function cyclicalEffect() {
+    front.style.transform = `rotateY(${180 + degree}deg)`;
+    back.style.transform = `rotateY(${360 + degree}deg)`;
+    degree += 180;
+}
